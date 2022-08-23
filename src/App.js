0d488b679c615e5c33a5cbbcf516a93e57761d78
Ali <100287885+AlisahibAmiraslan@users.noreply.github.com>
2022-08-23 13:55:05 +0300

    dsd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import About from "./About/About";
 import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Home from "./Home/Home";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+	const location = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location]);
+
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="contact" element={<Contact />} />
+			</Routes>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
